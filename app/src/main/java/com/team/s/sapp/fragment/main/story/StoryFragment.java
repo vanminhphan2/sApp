02493a246a9a30b67ee1128d006f34c9513fd48c
trantheho <nasu120696@gmail.com>
2018-08-12 23:04:13 +0700

    Merge branch 'master' of C:\Users\User\Desktop\sApp with conflicts.

--- app/src/main/java/com/team/s/sapp/fragment/main/story/StoryFragment.java
+++ app/src/main/java/com/team/s/sapp/fragment/main/story/StoryFragment.java
@@ -7,15 +7,25 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.team.s.sapp.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
 public class StoryFragment extends Fragment {
+
+    Unbinder unbinder;
+    @BindView(R.id.img_bg_story)
+    ImageView imgbg;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view= inflater.inflate(R.layout.fragment_story, container, false);
+        unbinder = ButterKnife.bind(this, view);
         return view;
     }
 }
